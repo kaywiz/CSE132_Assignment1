931a411ea907dec63c281855a43c529def9146c8
cse132@SEASAD.WUSTL.EDU <cse132@SEASAD.WUSTL.EDU@5079ff04-d25d-4c85-8423-372005a2952e>
2016-01-27 06:33:38 +0000

    Import: from images assign1//c to repo k.wisneski at node = /c

---
+++ cse132_sp16/c/assignment1/manipFunctions.c
@@ -0,0 +1,36 @@
+/* 
+ * manipFunctions.c
+ * 
+ * This source file defines all the functions used to bit manipulate
+ *
+ */
+
+#include "manipFunctions.h"
+
+int hasAOne(int num) {
+  return 0;
+}
+
+int hasAZero(int num) {
+  return 0;
+}
+
+int leastSigHasAOne(int num) {
+  return 0;
+}
+
+int negate(int num) {
+  return 0;
+}
+
+int isNegativeInt(int num) {
+  return 0;
+}
+
+int isNegativeLong(long num) {
+  return 0;
+}
+
+int isNegativeChar(char num) {
+  return 0;
+}
